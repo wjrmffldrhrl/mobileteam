42e3e8b85502be6a7f0c574960281986837bc094
wjrmffldrhrl <49122299+wjrmffldrhrl@users.noreply.github.com>
2019-04-06 20:15:24 +0900

    성공적으로 옮겼으나 메인에서 OcrDetectorActivity로 넘어갈때 오류가 생김 OcrDetectorActivity로 화면을 시작해도 어플이 종료되는것을 봐서는 해당 엑티비티에 오류가 있는것으로 파악됨

--- app/src/main/java/com/example/annu/MainActivity.java
+++ app/src/main/java/com/example/annu/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.annu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt1 = (Button) findViewById(R.id.main_bt1);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OcrCaptureActivity.class);//인텐트 지정
+                startActivity(intent);//액티비티 출력
+            }
+        });
     }
 }
