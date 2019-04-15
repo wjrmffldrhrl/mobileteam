ed9476882cb16cf9bdae74ebaaeb5d4ffd3f41c8
cats0713 <49222407+cats0713@users.noreply.github.com>
2019-04-15 13:25:17 +0900

    test

--- app/src/main/java/com/example/annu/MainActivity.java
+++ app/src/main/java/com/example/annu/MainActivity.java
@@ -10,30 +10,12 @@ import com.example.annu.EyeDetected.EyeDetected;
 import com.example.annu.OCR.OcrCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button bt1,bt2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt1 = (Button) findViewById(R.id.main_bt1);
-        bt2 = (Button) findViewById(R.id.main_bt2);
 
-
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), OcrCaptureActivity.class);//인텐트 지정
-                startActivity(intent);//액티비티 출력
-            }
-        });
-
-        bt2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), EyeDetected.class);//인텐트 지정
-                startActivity(intent);//액티비티 출력
-            }
-        });
     }
 }
